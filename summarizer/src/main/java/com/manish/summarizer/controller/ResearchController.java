9982fc56c76eded85251a48c1f315a1f117bb04d
Manish-061 <kumarmanish092003@gmail.com>
2025-12-04 12:06:17 +0530

    configured Gemini Response and completed ResearchService

--- summarizer/src/main/java/com/manish/summarizer/controller/ResearchController.java
+++ summarizer/src/main/java/com/manish/summarizer/controller/ResearchController.java
@@ -8,6 +8,15 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/research")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
 public class ResearchController {
+    private final ResearchService researchService;
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processContent(@RequestBody ResearchRequest request){
+        String result = researchService.processContent(request);
+        return ResponseEntity.ok(result);
+    }
 
 }
--- summarizer/src/main/java/com/manish/summarizer/service/ResearchService.java
+++ summarizer/src/main/java/com/manish/summarizer/service/ResearchService.java
@@ -1,10 +1,83 @@
 package com.manish.summarizer.service;
 
+import com.manish.summarizer.model.GeminiResponse;
 import com.manish.summarizer.model.ResearchRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import tools.jackson.databind.ObjectMapper;
 
+import java.util.Map;
+
+@Service
 public class ResearchService {
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    private final WebClient webClient;
+    private final ObjectMapper objectMapper;
+
+    public ResearchService(WebClient.Builder webClientBuilder, ObjectMapper objectMapper) {
+        this.webClient = webClientBuilder.build();
+        this.objectMapper = objectMapper;
+    }
+
     public String processContent(ResearchRequest request) {
-        return "";
+        String prompt = buildPrompt(request);
+
+        //query the AI model
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[] {
+                        Map.of("parts", new Object[]{
+                                Map.of("text", prompt)
+                        })
+                }
+        );
+        String response = webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return extractTextFromResponse(response);
+    }
+
+    private String extractTextFromResponse(String response) {
+        try{
+            GeminiResponse geminiResponse = new ObjectMapper().readValue(response, GeminiResponse.class);
+            if(geminiResponse.getCandidates() != null && !geminiResponse.getCandidates().isEmpty()){
+                GeminiResponse.Candidate firstCandidate = geminiResponse.getCandidates().get(0);
+                if(firstCandidate.getContent() != null &&
+                        firstCandidate.getContent().getParts() != null &&
+                        !firstCandidate.getContent().getParts().isEmpty()){
+                    return firstCandidate.getContent().getParts().get(0).getText();
+                }
+            }
+            return "No response found.";
+        } catch (Exception e){
+            return "Error parsing response: " + e.getMessage();
+        }
+    }
+
+    // build prompt
+    private String buildPrompt(ResearchRequest request){
+        StringBuilder prompt = new StringBuilder();
+        switch(request.getOperation()){
+            case "summarize":
+                prompt.append("Summarize the following text: ");
+                break;
+            case "suggest":
+                prompt.append("Paraphrase the following text: ");
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown operation: " + request.getOperation());
+        }
+        prompt.append(request.getContent());
+        return prompt.toString();
     }
 }
  
